be8d368fad6ca9a883385f5cea57f6cdff9fa5be
Julianna Harwood <julianna.harwood@latimes.com>
2025-01-15 00:33:36 -0800

    change to pickles, add dropdown menu for serve, clean up code

--- backend/run_prediction.py
+++ backend/run_prediction.py
@@ -7,10 +7,8 @@ def predict_from_model(model_path, new_data) :
     model = joblib.load(model_path)
 
     # check inputs and return errors
-    if (new_data['becca_partner'] == new_data['julianna_partner']) and (new_data['becca_partner'] != ''):
+    if (new_data['becca_partner'].rsplit('_', 1)[0] == new_data['julianna_partner'].rsplit('_', 1)[0]) and (new_data['becca_partner'] != ''):
         return "Must choose different partners."
-    if new_data['serve'] == '':
-        return "Must choose Julianna or Becca to serve first."
     
     # Set variables
     if new_data['becca_partner'] == '' :
@@ -20,7 +18,7 @@ def predict_from_model(model_path, new_data) :
         new_data['julianna_partner'] = 'None'
     
     # Create data for model (start with non-dummy vars)
-    X = pd.DataFrame({'game_num': new_data['game_num'], 'becca_start': [True if new_data['serve'] == "becca" else False]})
+    X = pd.DataFrame({'game_num': new_data['game_num'], 'becca_start': [True if new_data['serve'] == "Becca" else False]})
 
     # Create dummies
     for feature in model.feature_names_in_.tolist() :
@@ -32,8 +30,8 @@ def predict_from_model(model_path, new_data) :
     # Fill in dummies
 
     # partners
-    julianna_partner = 'Player_A_2_' + new_data['julianna_partner'].capitalize()
-    becca_partner = 'Player_B_2_' + new_data['becca_partner'].capitalize()
+    julianna_partner = 'Player_A_2_' + new_data['julianna_partner'].rsplit('_', 1)[0].capitalize()
+    becca_partner = 'Player_B_2_' + new_data['becca_partner'].rsplit('_', 1)[0].capitalize()
 
     X[julianna_partner] = 1
     X[becca_partner] = 1
