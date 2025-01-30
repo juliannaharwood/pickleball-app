eb90da72ecb6fadd623575c0cba04f10fec559e6
Julianna Harwood <julianna.harwood@latimes.com>
2025-01-29 22:09:33 -0800

    fix mobile, add notebook and about button, remove password

--- app.py
+++ app.py
@@ -6,28 +6,30 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/authenticate', methods=['POST'])
-def authenticate():
-    # Get the password entered by the user
-    password = request.form['password']
-    
-    # Hardcoded password (replace this with your actual password)
-    correct_password = 'jojosiwa'
-    
-    # Validate the password
-    if password == correct_password:
-        # Redirect to the protected page if the password is correct
-        return redirect(url_for('pickleball_predictor'))
-    else:
-        # Redirect back to the login page with a message if the password is incorrect
-        return redirect(url_for('index'))
-
-# main page
-@app.route('/pickleball-predictor')
-def pickleball_predictor():
     return render_template('main.html')
+
+# if a password is needed, replace main.html above with index.html and uncomment below
+
+# @app.route('/authenticate', methods=['POST'])
+# def authenticate():
+#     # Get the password entered by the user
+#     password = request.form['password']
+    
+#     # Hardcoded password (replace this with your actual password)
+#     correct_password = 'jojosiwa'
+    
+#     # Validate the password
+#     if password == correct_password:
+#         # Redirect to the protected page if the password is correct
+#         return redirect(url_for('pickleball_predictor'))
+#     else:
+#         # Redirect back to the login page with a message if the password is incorrect
+#         return redirect(url_for('index'))
+
+# # main page
+# @app.route('/pickleball-predictor')
+# def pickleball_predictor():
+#     return render_template('main.html')
 
 # predict winner
 @app.route('/predict', methods=['POST'])
--- backend/utils.py
+++ backend/utils.py
@@ -2,21 +2,25 @@ import numpy as np
 import pandas as pd
 
 def load_data(path):
-    # Load data from CSV file into a DataFrame
+    """
+    Load data
+    """
     data = pd.read_csv(path, header = None)
     return(data)
 
 def clean_data(data):
+    """
+    Do necessary cleaning
+    """
     # clean header
     header = data.iloc[1]
-    header.iloc[-2:] = ['Paddle2','Incomplete'] # set last two column names
+    header.iloc[-2:] = ['Paddle2', 'Incomplete'] # set last two column names
+    
     clean_data = data.iloc[2:]
     clean_data.columns = header
 
     # rename cols
-    clean_data = clean_data.rename(columns = {'Court Time': 'court_time',
-                                        'Game End Times': 'game_end_times',
-                                        'Game #':'game_num'})
+    clean_data = clean_data.rename(columns = {'Court Time': 'court_time', 'Game End Times': 'game_end_times', 'Game #':'game_num'})
 
     # Fill nas for empty partners and incomplete
     clean_data.Player_A_2 = clean_data.Player_A_2.fillna("None")
@@ -48,7 +52,7 @@ def clean_data(data):
     clean_data.Paddle2 = clean_data.Paddle2.apply(lambda x: x.strip())
 
     # Add missing game numbers
-    clean_data.game_num = clean_data.groupby(['Date']).cumcount()+1
+    clean_data.game_num = clean_data.groupby(['Date']).cumcount() + 1
 
     # Remove incomplete and games that B/J weren't on opposing teams
     clean_data = clean_data[(clean_data['Player_A_1'] == 'Julianna') & (clean_data['Player_B_1'] == 'Becca')]
@@ -57,20 +61,26 @@ def clean_data(data):
     return(clean_data)
 
 def feature_engineer(data):
-    # Create game length variable
-    data['game_length'] = data.groupby(['Date']).game_end_times.diff().fillna(data.game_end_times - data.court_time)
+    """
+    Create features for model
+    """
+    # Create game length variables
+    data['game_length'] = data.groupby(['Date']).game_end_times.diff().fillna(data.game_end_times - data.court_time)  # first game will be end time - court start time
+    data['game_length_mins'] = data.game_length.dt.total_seconds()/60
     
     # Create serve variable
     data['becca_start'] = np.where(data['Start_2']== 'Y', True, False)
 
-    # Create year/month variables
+    # Create year, month, hour variables
     data['year'] = data.Date.dt.year
+    data['month'] = data.Date.dt.month
+    data['hour'] = data.court_time.dt.hour
 
     # Create winner variable (outcome)
     data['becca_win'] = data.Score_1 < data.Score_2
 
     # drop unneeded vars
-    data = data.drop(['Date','game_end_times','Start_1','Start_2','Score_1','Score_2','Incomplete'], axis = 1)
+    data = data.drop(['Date', 'game_end_times', 'Start_1', 'Start_2', 'Score_1', 'Score_2', 'Incomplete'], axis = 1)
 
     return(data)
 
